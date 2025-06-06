60a92b08b6b10dfc8b606cae84414f5a0e54b03a
OldM1lk <MrlaimWin@gmail.com>
2025-06-06 20:08:37 +0700

    Change dropdown menu for working with different types of data

--- app/src/main/java/com/example/todo/feature_todo/presentation/add_edit_todo/components/DropDown.kt
+++ app/src/main/java/com/example/todo/feature_todo/presentation/add_edit_todo/components/DropDown.kt
@@ -3,55 +3,61 @@ package com.example.todo.feature_todo.presentation.add_edit_todo.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ExposedDropdownMenuBox
-import androidx.compose.material.Text
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.todo.feature_todo.domain.model.Todo
-import com.example.todo.feature_todo.presentation.add_edit_todo.AddEditTodoEvent
-import com.example.todo.feature_todo.presentation.add_edit_todo.AddEditTodoViewModel
 
 @Composable
 @OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
-fun PriorityDropDownMenu(
-    viewModel: AddEditTodoViewModel = hiltViewModel()
+fun DropDown(
+    label: String,
+    options: List<String>,
+    onSelect: (String) -> Unit,
 ) {
 
-    val todoState = viewModel.todoState.collectAsStateWithLifecycle()
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    var selectedOption by remember { mutableStateOf(options.first()) }
 
     ExposedDropdownMenuBox(
-        expanded = todoState.value.isPriorityDropDownMenuExpanded,
+        expanded = expanded,
         onExpandedChange = {
-            viewModel.onEvent(AddEditTodoEvent.OnPriorityDropDownMenuExpandedChange(it))
+            expanded = it
         },
         modifier = Modifier
             .fillMaxWidth()
     ) {
         OutlinedTextField(
-            value = todoState.value.currentTodoPriority,
+            value = selectedOption,
             onValueChange = {},
             readOnly = true,
-            label = { Text("Priority") },
+            label = { Text(label) },
             trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = todoState.value.isPriorityDropDownMenuExpanded
-                )
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .menuAnchor(
+                    type = MenuAnchorType.SecondaryEditable,
+                    enabled = true
+                )
         )
         ExposedDropdownMenu(
-            expanded = todoState.value.isPriorityDropDownMenuExpanded,
-            onDismissRequest = {
-                viewModel.onEvent(AddEditTodoEvent.HidePriorityDropDownMenu)
-            }
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
         ) {
             Todo.todoPriorities.forEach {
                 DropdownMenuItem(
@@ -62,8 +68,9 @@ fun PriorityDropDownMenu(
                         )
                     },
                     onClick = {
-                        viewModel.onEvent(AddEditTodoEvent.OnPriorityChange(it))
-                        viewModel.onEvent(AddEditTodoEvent.HidePriorityDropDownMenu)
+                        selectedOption = it
+                        onSelect(it)
+                        expanded = false
                     },
                     modifier = Modifier
                         .fillMaxWidth()
